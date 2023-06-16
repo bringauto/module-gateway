db00366f129c10d535ba263411340c0c312e08e4
Jiri Zak <jiri.zak@bringauto.com>
2023-06-16 09:50:57 +0200

    update comments

--- include/bringauto/external_client/connection/ExternalConnection.hpp
+++ include/bringauto/external_client/connection/ExternalConnection.hpp
@@ -19,15 +19,20 @@ public:
 
     void send();
 private:
-	int messageCounter_ {};
+
+	int messageCounter_ = 0;
+
 	std::string sessionId_ {};
+
 	std::unique_ptr<communication::ICommunicationChannel> communicationChannel_ {};
 	// Settings - TODO save only context and extract this
 
 	// std::vector<int> modules_; // TODO change to map aggregators?
 
 	std::string carId_ {}; // TODO not needed
+
 	std::string vehicleName_ {};
+
 	std::string company_ {};
 };
 
