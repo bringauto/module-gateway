b3613b743244820efa0f1c2c63eacc67d64fc995
Jiri Zak <jiri.zak@bringauto.com>
2023-06-16 14:43:54 +0200

    Add mqtt to cmake

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -5,6 +5,8 @@ BA_PACKAGE_LIBRARY(nlohmann-json v3.10.5 PLATFORM_STRING_MODE any_machine NO_DEB
 BA_PACKAGE_LIBRARY(cxxopts       v3.0.5  PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(boost      v1.78.0)
 BA_PACKAGE_LIBRARY(ba-logger     v1.2.0)
+BA_PACKAGE_LIBRARY(pahomqttc     v1.3.9)
+BA_PACKAGE_LIBRARY(pahomqttcpp   v1.2.0)
 
 IF (BRINGAUTO_TESTS)
     BA_PACKAGE_LIBRARY(gtest         v1.12.1)
