b4bd01aa5ec15d656e81da089876c64592d004ac
MarioIvancik <mario.ivancik@bringauto.com>
2025-07-22 09:52:19 +0200

    dont move const references; remade logger id macros to a constexpr function

--- include/bringauto/settings/LoggerId.hpp
+++ include/bringauto/settings/LoggerId.hpp
@@ -2,30 +2,39 @@
 
 #include <bringauto/settings/LoggerWrapper.hpp>
 
-#ifndef MINIMUM_LOGGER_VERBOSITY
-#define MINIMUM_LOGGER_VERBOSITY 0 // DEBUG
-#endif
-
-#if MINIMUM_LOGGER_VERBOSITY == 0 // DEBUG
-#define LOGGER_VERBOSITY bringauto::logging::LoggerVerbosity::Debug
-#elif MINIMUM_LOGGER_VERBOSITY == 1 // INFO
-#define LOGGER_VERBOSITY bringauto::logging::LoggerVerbosity::Info
-#elif MINIMUM_LOGGER_VERBOSITY == 2 // WARNING
-#define LOGGER_VERBOSITY bringauto::logging::LoggerVerbosity::Warning
-#elif MINIMUM_LOGGER_VERBOSITY == 3 // ERROR
-#define LOGGER_VERBOSITY bringauto::logging::LoggerVerbosity::Error
-#elif MINIMUM_LOGGER_VERBOSITY == 4 // CRITICAL
-#define LOGGER_VERBOSITY bringauto::logging::LoggerVerbosity::Critical
-#else
-#error "Invalid MINIMUM_LOGGER_VERBOSITY defined. Use DEBUG(0), INFO(1), WARNING(2), ERROR(3), or CRITICAL(4)."
+#ifndef MODULE_GATEWAY_MINIMUM_LOGGER_VERBOSITY
+#define MODULE_GATEWAY_MINIMUM_LOGGER_VERBOSITY 0 // DEBUG
 #endif
 
 
 
 namespace bringauto::settings {
 
+static_assert(MODULE_GATEWAY_MINIMUM_LOGGER_VERBOSITY >= 0 && MODULE_GATEWAY_MINIMUM_LOGGER_VERBOSITY <= 4,
+			  "Invalid MINIMUM_LOGGER_VERBOSITY defined. Use DEBUG(0), INFO(1), WARNING(2), ERROR(3), or CRITICAL(4).");
+
+/**
+ * @brief Converts the minimum logger verbosity defined in CMake to LoggerVerbosity enum.
+ * @param verbosityNumber The minimum logger verbosity defined in CMake.
+ */
+constexpr logging::LoggerVerbosity toLoggerVerbosity(uint8_t verbosityNumber) {
+	switch (verbosityNumber) {
+		case 1:
+			return logging::LoggerVerbosity::Info;
+		case 2:
+			return logging::LoggerVerbosity::Warning;
+		case 3:
+			return logging::LoggerVerbosity::Error;
+		case 4:
+			return logging::LoggerVerbosity::Critical;
+		case 0:
+		default:
+			return logging::LoggerVerbosity::Debug;
+	}
+}
+
 constexpr logging::LoggerId logId = {.id = "ModuleGateway"};
 using BaseLogger = logging::Logger<logId, logging::LoggerImpl>;
 
-using Logger = LoggerWrapper<BaseLogger, LOGGER_VERBOSITY>;
+using Logger = LoggerWrapper<BaseLogger, toLoggerVerbosity(MODULE_GATEWAY_MINIMUM_LOGGER_VERBOSITY)>;
 }
