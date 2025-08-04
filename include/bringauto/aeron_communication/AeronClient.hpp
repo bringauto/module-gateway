dd4575efc756fa14be05dee0b07e3ae29499987f
MarioIvancik <mario.ivancik@bringauto.com>
2025-08-04 10:46:57 +0200

    added docstrings for AeronClient

--- include/bringauto/aeron_communication/AeronClient.hpp
+++ include/bringauto/aeron_communication/AeronClient.hpp
@@ -8,6 +8,10 @@
 
 namespace bringauto::aeron_communication {
 
+/**
+ * This class is responsible for managing Aeron communication.
+ * It is used for calling module functions on a separate module binary using Aeron.
+ */
 class AeronClient {
 
 public:
@@ -23,14 +27,35 @@ public:
 		STATUS_DATA_VALID = 8
 	};
 
+	/**
+	 * @brief Constructs an AeronClient instance.
+	 * Initializes the Aeron context and sets up handlers for publications and subscriptions.
+	 */
 	AeronClient();
 
 	~AeronClient() = default;
 
+	/**
+	 * @brief Adds a module to the Aeron client with the given module ID.
+	 * Creates a publication and subscription for the module.
+	 * Only supports one module for now.
+	 * @param moduleId The ID of the module to be added.
+	 */
 	void addModule(uint16_t moduleId);
 
+	/**
+	 * @brief Calls a module function with the specified message.
+	 * Constructs a full message by combining the function code and the provided message,
+	 * then sends it via Aeron publication.
+	 * @param function The module function to call.
+	 * @param message The message to send to the module.
+	 */
 	void callModuleFunction(ModuleFunctions function ,const std::string &message);
 
+	/**
+	 * @brief Retrieves the last received message from Aeron.
+	 * @return A string view of the last message received.
+	 */
 	std::string_view getMessage() const;
 
 private:
@@ -45,7 +70,9 @@ private:
 	std::shared_ptr<aeron::FragmentAssembler> aeronFragmentAssembler_ {nullptr};
 	std::shared_ptr<aeron::fragment_handler_t> aeronHandler_ {nullptr};
 	std::shared_ptr<aeron::YieldingIdleStrategy> aeronIdleStrategy_ {nullptr};
+	/// Indicates if the Aeron client is currently polling for messages
 	std::atomic<bool> aeronPolling_ {false};
+	/// Last message received from Aeron
 	std::string aeronMessage_ {};
 
 };
