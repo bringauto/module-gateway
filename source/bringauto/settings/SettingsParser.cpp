93a9c8d5f56eba339ed8946f637eeff9b5655ad6
Vladislav Rýgl <vladislav.rygl@bringauto.com>
2025-12-03 14:36:18 +0100

    Check if module defined in external-connection endpoint exists in module-paths

--- source/bringauto/settings/SettingsParser.cpp
+++ source/bringauto/settings/SettingsParser.cpp
@@ -109,15 +109,18 @@ bool SettingsParser::areSettingsCorrect() const {
 		isCorrect = false;
 	}
 
-	for(auto& externalConnectionSettings: settings_->externalConnectionSettingsList) {
-		for(auto const& externalModuleId: externalConnectionSettings.modules) {
-			if(!settings_->modulePaths.contains(externalModuleId)) {
+	std::ranges::any_of(settings_->externalConnectionSettingsList, [&](auto& externalConnectionSettings){
+		return std::ranges::any_of(externalConnectionSettings.modules, [&](auto const& externalModuleId) {
+			bool isMissing = !settings_->modulePaths.contains(externalModuleId);
+			if (isMissing)
+			{
 				std::cerr << "Module " << externalModuleId <<
 				" is defined in external-connection endpoint modules but is not specified in module-paths" << std::endl;
 				isCorrect = false;
 			}
-		}
-	}
+			return isMissing;
+		});
+	});
 
 	return isCorrect;
 }
