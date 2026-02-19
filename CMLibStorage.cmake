d0e9cc8e52faa12155f1ef14c51f34768cb23159
jiri-skuta <jiri.skuta@bringauto.com>
2026-02-19 14:22:47 +0100

    Added CMCONF init Increased version of cxxopts from v3.0.5 to v3.1.1 Increased version of nlohmann_json from v3.2.0 to v3.10.5

--- CMLibStorage.cmake
+++ CMLibStorage.cmake
@@ -1,3 +1,4 @@
 SET(STORAGE_LIST DEP)
 
-SET(STORAGE_LIST_DEP "https://github.com/bacpack-system/package-tracker.git")
+SET(STORAGE_LIST_DEP          "https://github.com/bacpack-system/package-tracker.git")
+SET(STORAGE_LIST_DEP_REVISION "v0.0.0" CACHE STRING "" FORCE)
